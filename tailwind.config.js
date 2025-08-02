b5bfd775344651f04377228d2b668cee72162f24
wqqhou <wqqmatt@yahoo.com.tw>
2025-08-02 16:23:50 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+    content: ["./templates/**/*.html"],
+    theme: {
+      extend: {
+        colors: {
+          'gradient-start': '#ffddc1',
+          'gradient-end': '#ffb3c1',
+        },
+        fontFamily: {
+          serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
