957f6dc826d03dcb9ceb86c60f1efd16ca95775d
wqqhou <wqqmatt@yahoo.com.tw>
2025-08-05 01:41:36 +0800

    Removed hardcoded private key

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ import re
 import json
 from fastapi.staticfiles import StaticFiles
 import random
+import os
 
 with open('questions.json') as f:
     METRIC_CONFIG = json.load(f)
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-app.add_middleware(SessionMiddleware, secret_key="supersecretkey")
+app.add_middleware(SessionMiddleware, secret_key=os.getenv("SESSION_SECRET_KEY"))
 templates = Jinja2Templates(directory="templates")
 templates.env.auto_reload = True  # Enable auto reload
 templates.env.cache = {}
